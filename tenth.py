3b34fe78c988f825924637649364573328b42beb
mohit <mohit9899553@gmail.com>
2026-06-03 15:04:41 +0530

    main funtcion

--- tenth.py
+++ tenth.py
@@ -49,37 +49,40 @@ def div(a,b):
 
 # main()
 
-def Add(a,b):
-    return a+b
+# def Add(a,b):
+#     return a+b
 
-def Sub(a,b):
-    return a-b
+# def Sub(a,b):
+#     return a-b
 
-def mul(a,b):
-    return a*b
+# def mul(a,b):
+#     return a*b
 
-def div(a,b):
-    if(b==0):
-        print('infinty')
-    else:
-        return a/b
+# def div(a,b):
+#     if(b==0):
+#         print('infinty')
+#     else:
+#         return a/b
     
 
-def main():
-    num1=int(input('enter the first number '))
-    num2=int(input('enter the second number '))
-    op=input('enter operator ')
+# def main():
+#     num1=int(input('enter the first number '))
+#     num2=int(input('enter the second number '))
+#     op=input('enter operator ')
 
-    if(op=='+'):
-        print(Add(num1,num2))
-    elif(op=='-'):
-        print(Sub(num1,num2))
-    elif(op=='*'):
-        print(mul(num1,num2))
-    elif(op=='/'):
-        print(div(num1,num2))
-    else:
-        print('valid number of operter ')
+#     if(op=='+'):
+#         print(Add(num1,num2))
+#     elif(op=='-'):
+#         print(Sub(num1,num2))
+#     elif(op=='*'):
+#         print(mul(num1,num2))
+#     elif(op=='/'):
+#         print(div(num1,num2))
+#     else:
+#         print('valid number of operter ')
 
 
-main()
+
+for x in range(1,100):
+    if(x%2!=0):
+        print('even',x)
