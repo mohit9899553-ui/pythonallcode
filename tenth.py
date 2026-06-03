6e7bd8ee0a81d11a6a408e77924cd891b7f2a4c9
mohit <mohit9899553@gmail.com>
2026-06-03 14:57:19 +0530

    main functuio

---
+++ tenth.py
@@ -0,0 +1,85 @@
+# main function
+# aaaaa?a
+
+def Add(a,b):
+    return a+b
+
+def Sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if(b==0):
+        print('infinty')
+    else:
+        return a/b
+    
+
+# def Clac():
+#     num1=int(input('enter the first number '))
+#     num2=int(input('enter the second number '))
+#     op=input('enter operator ')
+
+#     if(op=='+'):
+#         print(Add(num1,num2))
+#     elif(op=='-'):
+#         print(Sub(num1,num2))
+#     elif(op=='*'):
+#         print(mul(num1,num2))
+#     elif(op=='/'):
+#         print(div(num1,num2))
+
+# Clac()
+
+# def main():
+#     num1=int(input('enter the first number '))
+#     num2=int(input('enter the second number '))
+#     op=input('enter operator ')
+
+#     if(op=='+'):
+#         print(Add(num1,num2))
+#     elif(op=='-'):
+#         print(Sub(num1,num2))
+#     elif(op=='*'):
+#         print(mul(num1,num2))
+#     elif(op=='/'):
+#         print(div(num1,num2))
+
+# main()
+
+def Add(a,b):
+    return a+b
+
+def Sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if(b==0):
+        print('infinty')
+    else:
+        return a/b
+    
+
+def main():
+    num1=int(input('enter the first number '))
+    num2=int(input('enter the second number '))
+    op=input('enter operator ')
+
+    if(op=='+'):
+        print(Add(num1,num2))
+    elif(op=='-'):
+        print(Sub(num1,num2))
+    elif(op=='*'):
+        print(mul(num1,num2))
+    elif(op=='/'):
+        print(div(num1,num2))
+    else:
+        print('valid number of operter ')
+
+
+main()
